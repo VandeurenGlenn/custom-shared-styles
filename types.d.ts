47f69b34cea9f4ec141a0adf5643c4dbb4b5a831
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-14 09:40:11 +0200

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,2 @@
-import { CSSResult } from '@lit/reactive-element/css-tag.js';
-
-export default CSSResult;
+declare const _default: import('@lit/reactive-element/css-tag.js').CSSResultOrNative;
+export default _default;
